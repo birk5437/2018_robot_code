b1b6da9e192690f85a5f2a6dee72cba2900da1bb
Burke Ramsey <birk5437@gmail.com>
2017-10-07 16:15:57 -0400

    burkes feature in progress

--- test_cool_thing.java
+++ test_cool_thing.java
@@ -1,5 +1,29 @@
 public class TestCoolThing
   public static void main(String args[]) {
-    System.out.println("this is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+        System.out.println("BURKE is a cool thing!");
+    System.out.println("BURKE is a cool thing!");
+    
   }
 }
